cebc56d97f84d6515f04a6304f3145ab5d4e5180
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2022-03-02 03:07:56 +0600

    working on hero section(responsiveness)

--- src/Components/Header/HeroSection/HeroSection.js
+++ src/Components/Header/HeroSection/HeroSection.js
@@ -11,7 +11,7 @@ const HeroSection = () => {
             <Container>
                 <Row className='border border-4' style={{ height: "100%" }} >
                     {/* this col will contain the text of this section and 2 buttons */}
-                    <Col lg={5} md={12} sm={12} className=" d-flex flex-column justify-content-center  text-start border border-1 " >
+                    <Col lg={5} className=" d-flex flex-column justify-content-center  text-start " >
                         <h1 style={{ color: "#152427" }} className="fw-bolder">We’re a <br />creative design <br /> agency.</h1>
                         <h6 className='fs-6' style={{ color: "#575D5E" }}>A brilliant, modular agency template for startup’s build yours toady.</h6>
                         <div>
@@ -24,29 +24,29 @@ const HeroSection = () => {
                     </Col>
                     {/* this secion will contain 1bg image and 3 image */}
                     {/* here i will use css-position property because here i will be using a picture as bg so using position property i can keep 3 images over the bg image */}
-                    <Col lg={7} md={4} sm={12} className="border border-2" style={{ position: "relative" }} >
-                        <div className="border-2  border d-flex justify-content-center .align-items-center">
+                    <Col lg={7} className="" style={{ position: "relative" }} >
+                        <div className=" d-flex justify-content-center .align-items-center">
                             <img alt="bg" src={bgimg} style={{ maxWidth: "100%", maxHeight: "100%" }} />
                         </div>
                         <Row>
                             <Col lg={3} md={12} style={{
                                 position: "absolute",
-                                top: "100px", left: "100px",
+                                top: "100px", left: "40px",
                             }}    >
-                                <img alt="heroimage" src={img1} style={{ width: "150px" }} />
+                                <img alt="heroimage" src={img1} style={{ maxWidth: "150px" }} />
                             </Col>
                             <Col lg={3} style={{
                                 position: "absolute",
                                 top: "150px",
-                                left: "300px"
+                                left: "200px"
                             }} >
-                                <img alt="heroimage" src={img2} style={{ width: "150px" }} />
+                                <img alt="heroimage" src={img2} style={{ maxWidth: "150px" }} />
                             </Col>
                             <Col lg={3} style={{
                                 position: "absolute",
-                                top: "100px", right: "40px"
+                                top: "100px", left: "360px"
                             }} >
-                                <img alt="heroimage" src={img3} style={{ width: "150px" }} />
+                                <img alt="heroimage" src={img3} style={{ maxWidth: "150px" }} />
                             </Col>
                         </Row>
                     </Col>
