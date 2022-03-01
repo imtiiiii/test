874061e1f9693f9e2642ec85ffe6da77e4753ad8
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2022-03-01 19:37:08 +0600

    working on navbar

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+// header component. This component will hold two more components which are navbar and hero section
+import React from 'react';
+const Header = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Header;
