1ce3bf29fcbb3a060f0122914f3fd24e45bede27
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2022-03-02 03:32:11 +0600

    done with header section

--- src/Components/Header/HeroSection/HeroSection.js
+++ src/Components/Header/HeroSection/HeroSection.js
@@ -12,13 +12,13 @@ const HeroSection = () => {
     return (
         <>
             <Container>
-                <Row className='border border-4' style={{ height: "100%" }} >
+                <Row className='' style={{ height: "700px" }} >
                     {/* this col will contain the text of this section and 2 buttons */}
-                    <Col lg={5} className=" d-flex flex-column justify-content-center  text-start " >
+                    <Col lg={5} className=" d-flex flex-column justify-content-center text-start " >
                         <h1 style={{ color: "#152427" }} className="fw-bolder d-none d-lg-block">We’re a <br />creative design <br /> agency.</h1>
                         <h3 style={{ color: "#152427" }} className="fw-bolder  d-lg-none">We’re a creative design  agency.</h3>
                         <h6 className='fs-6' style={{ color: "#575D5E" }}>A brilliant, modular agency template for startup’s build yours toady.</h6>
-                        <div>
+                        <div className='my-5'>
                             <Button variant='signup rounded-pill fs-6 py-3 px-5 signup fw-bold'>Get in touch</Button>
                             <Button variant='signup rounded-pill fs-5 py-3 px-5  fw-bold ' style={{ color: "black", backgroundColor: "#fff" }}>
                                 <img src={playButton} alt="playbuttonlogo" className='mx-3' />
@@ -28,7 +28,7 @@ const HeroSection = () => {
                     </Col>
                     {/* this secion will contain 1bg image and 3 image */}
                     {/* here i will use css-position property because here i will be using a picture as bg so using position property i can keep 3 images over the bg image */}
-                    <Col lg={7} className="border border-2" style={{ position: "relative" }} >
+                    <Col lg={7} className="" style={{ position: "relative" }} >
                         <div className=" d-flex justify-content-center .align-items-center">
                             <img alt="bg" src={bgimg} style={{ maxWidth: "100%", maxHeight: "100%" }} />
                         </div>
@@ -38,7 +38,7 @@ const HeroSection = () => {
                                 top: "100px", left: "40px",
                             }}    >
                                 <img alt="heroimage" src={img1} style={{ maxWidth: "150px" }} className="d-lg-block d-none" />
-                                <img alt="heroimage" src={img4} style={{ maxWidth: "100px" }} className="d-lg-none d-block" />
+                                <img alt="heroimage" src={img4} style={{ maxWidth: "100px", maxHeight: "200px" }} className="d-lg-none d-block" />
                             </Col>
                             <Col lg={3} style={{
                                 position: "absolute",
@@ -46,14 +46,14 @@ const HeroSection = () => {
                                 left: "200px"
                             }} >
                                 <img alt="heroimage" src={img2} style={{ maxWidth: "150px" }} className="d-lg-block d-none" />
-                                <img alt="heroimage" src={img5} style={{ maxWidth: "100px" }} className="d-lg-none d-block" />
+                                <img alt="heroimage" src={img5} style={{ maxWidth: "100px", maxHeight: "200px" }} className="d-lg-none d-block" />
                             </Col>
                             <Col lg={3} style={{
                                 position: "absolute",
                                 top: "100px", left: "360px"
                             }} >
                                 <img alt="heroimage" src={img3} style={{ maxWidth: "150px" }} className="d-lg-block d-none" />
-                                <img alt="heroimage" src={img6} style={{ maxWidth: "100px" }} className="d-block d-lg-none" />
+                                <img alt="heroimage" src={img6} style={{ maxWidth: "100px", maxHeight: "200px" }} className="d-block d-lg-none" />
                             </Col>
                         </Row>
                     </Col>
