ccf98093ebd85f76df056f6587fb7db6941b18e7
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2022-03-02 17:23:10 +0600

    working on 2nd row of process section

--- src/Components/Processes/Processes.js
+++ src/Components/Processes/Processes.js
@@ -10,10 +10,10 @@ const Processes = () => {
                     <h3 className='fs-4 fw-bold' style={{ color: "#152427" }}>A simple ,yet effective three step process</h3>
                 </div>
                 <Container >
-                    {/* this row is only for the 01 point */}
-                    <Row className=' d-flex ' style={{ backgroundColor: "#E2EDDF", marginTop: "100px", borderRadius: "20px" }}>
-                        <Col lg={3} className="d-flex justify-content-between ">
-                            <div className='d-flex flex-column justify-content-center'>
+                    {/* this row is only for  01 row */}
+                    <Row className=' d-flex justify-content-between ' style={{ backgroundColor: "#E2EDDF", marginTop: "100px", borderRadius: "20px", border: "2px solid red" }}>
+                        <Col lg={4} className="d-flex justify-content-between  ">
+                            <div className='d-flex flex-column justify-content-center '>
                                 <h1 className=' fw-bold' style={{ color: "#152427", fontSize: "80px" }}>01.</h1>
                             </div>
                             <div className='d-flex flex-column justify-content-center  '>
@@ -21,26 +21,26 @@ const Processes = () => {
                                 <h4 className='fs-5 text-decoration-underline ' style={{ color: "#575D5E" }}>Learn More</h4>
                             </div>
                         </Col>
-                        <Col lg={9}>
+                        <Col lg={6} className="">
                             <div className=' d-flex justify-content-end' >
-                                <img alt="img" src={processimg} style={{ marginTop: "-250px" }} className="" />
+                                <img alt="img" src={processimg} style={{ marginTop: "-250px", border: "2px solid red" }} className="" />
                             </div>
                         </Col>
 
                     </Row>
                     {/* 2nd Row */}
-                    <Row className=' d-flex ' style={{ backgroundColor: "#E2EDDF", marginTop: "100px", borderRadius: "20px" }}>
-                        <Col lg={3} className="d-flex justify-content-between ">
-                            <div className='d-flex flex-column justify-content-center border border-2'>
+                    <Row className=' d-flex  justify-content-between ' style={{ marginTop: "100px", borderRadius: "20px", border: "2px solid red" }}>
+                        <Col lg={4} className="d-flex justify-content-between ">
+                            <div className='d-flex flex-column justify-content-center'>
                                 <h1 className=' fw-bold' style={{ color: "#152427", fontSize: "80px" }}>02.</h1>
                             </div>
-                            <div className='d-flex flex-column justify-content-center border border-2  '>
+                            <div className='d-flex flex-column justify-content-center   '>
                                 <h3 className='fs-4 fw-bold' style={{ color: "#152427" }}>Brainstorming</h3>
                             </div>
                         </Col>
                         <Col lg={6}>
-                            <div className=' d-flex justify-content-end border border-2' >
-                                <h5>We customse a ensure aligment and difine key mileones and defiverables experience tells us the best solutions are the ones  deated with clients.</h5>
+                            <div className=' d-flex justify-content-end ' >
+                                <h5 className='fs-5' style={{ color: "#7D8188" }}>We customse a ensure aligment and difine key mileones and defiverables experience tells us the best solutions are the ones  deated with clients.</h5>
                             </div>
                         </Col>
 
