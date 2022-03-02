920eb759f9efcd44532d1f12ee8a771b6a4cf5a0
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2022-03-02 13:39:03 +0600

    service section for pc version done

---
+++ src/Components/Services/Service/Service.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+
+const Service = (props) => {
+    const img = props.item.img;
+    console.log(img);
+    const title = props.item.title;
+    const details = props.item.details;
+    const cnt = props.count;
+    return (
+        <>
+            <Col lg={4} >
+                <div className='' style={{}}>
+                    <img alt="service" src={img} style={{ width: "100%" }} />
+                </div>
+                <div className={cnt === 3 ? 'shadow rounded-3 py-4' : "py-4"}>
+                    <div className='p-4' style={{ color: "#152427", height: "80px" }}>
+                        <h6 className='fs-4 fw-bold'>{title}</h6>
+                    </div>
+                    <div style={{ color: "#7D8188", height: "100px" }} className="p-4">
+                        <h6 className='fs-5'>{details}</h6>
+                    </div>
+                </div>
+            </Col>
+        </>
+    );
+};
+
+export default Service;
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import leftarrow from '../../img/services/leftarrow.png'
 import rightarrow from '../../img/services/rightarrow.png'
+import service1 from '../../img/services/service1.png'
+import service2 from '../../img/services/service2.png'
+import service3 from '../../img/services/service3.png'
+import Service from './Service/Service';
 const Services = () => {
+    let cnt = 1;
+    const services = [
+        {
+            img: "https://i.ibb.co/XZ1JH4B/service1.png",
+            title: "Branding",
+            details: "Create beautiful Agency website with Boluethat converts more visitors than any website."
+        },
+        {
+            img: "https://i.ibb.co/PmXR4JK/service2.png",
+            title: "Web Design",
+            details: "Create beautiful Agency website with Boluethat converts more visitors than any website."
+        },
+        {
+            img: "https://i.ibb.co/X8DpgFZ/service3.png",
+            title: "Graphic Design",
+            details: "Create beautiful Agency website with Boluethat converts more visitors than any website."
+        }
+    ];
     return (
         <>
             <Container className='my-5' >
@@ -14,8 +36,14 @@ const Services = () => {
                     <div className='d-flex justify-content-end align-items-end  w-50 '>
                         <img alt="leftarrow" width={54} height={54} src={leftarrow} />
                         <img alt="rightarrow" width={54} height={54} src={rightarrow} />
-
                     </div>
+                </Container>
+                <Container>
+                    <Row>
+                        {
+                            services.map(service => <Service item={service} key={cnt} count={cnt++}  ></Service>)
+                        }
+                    </Row>
                 </Container>
 
             </Container>
