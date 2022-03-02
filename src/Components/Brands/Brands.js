3c22401c2bd1eeff84ee45bdde0848eb1dbc56fd
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2022-03-02 11:34:11 +0600

    brand  section ok for now

--- src/Components/Brands/Brands.js
+++ src/Components/Brands/Brands.js
@@ -9,31 +9,31 @@ import heroku from '../../img/brands/heroku.png'
 const Brands = () => {
     return (
         <>
-            <Container fluid className='border border-4'>
-                <Container className='border border-1 rounded-pill py-5 ' style={{ backgroundColor: "#E2EDDF" }}>
 
-                    <Row className=' d-flex justify-content-center py-2 align-items-center'>
-                        <Col lg={2} className="py-2 ">
-                            <img alt="brands" src={google} className="mx-3 " style={{ widhth: "107px", height: "40px" }} />
-                        </Col>
-                        <Col lg={2} className="py-2">
-                            <img alt="brands" src={slack} className="mx-3" style={{ widhth: "86px", height: "50px" }} />
-                        </Col>
-                        <Col lg={2} className="py-2">
-                            <img alt="brands" src={agworld} className="mx-3" style={{ widhth: "107px", height: "40px" }} />
-                        </Col>
-                        <Col lg={3} className="py-2">
-                            <img alt="brands" src={riotinto} className="mx-3" style={{ widhth: "140px", height: "40px" }} />
-                        </Col>
-                        <Col lg={2} className="py-2" >
-                            <img alt="brands" src={heroku} className="mx-3" style={{ widhth: "80px", height: "30px" }} />
-                        </Col>
+            <Container className=' w-50   py-5 my-5 rounded-pill ' style={{ backgroundColor: "#E2EDDF" }}>
+
+                <Row className='  d-flex justify-content-center py-2 align-items-center'>
+                    <Col lg={2} md={6} sm={6} className="py-2 ">
+                        <img alt="brands" src={google} className="mx-3 " />
+                    </Col>
+                    <Col lg={2} md={6} sm={6} className="py-2">
+                        <img alt="brands" src={slack} className="mx-3" />
+                    </Col>
+                    <Col lg={2} md={6} sm={6} className="py-2">
+                        <img alt="brands" src={agworld} className="mx-3" />
+                    </Col>
+                    <Col lg={3} md={6} sm={6} className="py-2">
+                        <img alt="brands" src={riotinto} className="mx-3" />
+                    </Col>
+                    <Col lg={2} md={6} sm={6} className="py-2" >
+                        <img alt="brands" src={heroku} className="mx-3" />
+                    </Col>
 
 
-                    </Row>
+                </Row>
 
-                </Container>
             </Container>
+
         </>
     );
 };
