a8934f6180920269b6ba344c2b3e1ad5a438eabe
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2022-03-03 03:15:13 +0600

    done with about us section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import bg from '../../img/about/Ellipse 4024.png'
+import img1 from '../../img/about/Rectangle 6046.png'
+import img2 from '../../img/about/Rectangle 6047.png'
+import img3 from '../../img/about/Group 1000004124.png'
 
 const About = () => {
     return (
@@ -8,11 +12,41 @@ const About = () => {
                 {/* there is two part in section  */}
                 <Row>
                     {/* 1st part */}
-                    <Col lg={6}>
+                    <Col lg={8} className="d-none d-lg-block" style={{
+                        position: "relative"
+                    }}>
+                        <div >
+                            <img alt="background" src={bg} style={{ width: "100%", }} />
+                        </div>
+                        <div style={{
+                            position: "absolute", top: "20%", left: "10%"
 
+
+                        }}>
+                            <img alt="img" src={img1} className="rounded-pill" style={{ width: "284px", height: "480px", objectFit: "fill" }} />
+                        </div>
+
+                        <div style={{
+                            position: "absolute",
+                            top: "10%",
+                            right: "20%"
+
+
+
+                        }}>
+                            <img alt="img" src={img2} className="rounded-pill" style={{ width: "286px", height: "480px", objectFit: "fill" }} />
+                        </div>
+                        <div style={{
+                            position: "absolute",
+                            top: "50%",
+                            right: "15%"
+
+                        }}>
+                            <img alt="img" src={img3} className="w-100" />
+                        </div>
                     </Col>
                     {/* 2nd part */}
-                    <Col lg={6}>
+                    <Col lg={4} sm={12} className="">
                         <h5 style={{ color: "#152427" }} className="my-5">ABOUT US</h5>
                         <h2 style={{ color: "#152427" }}>We are an extention of <br /> your creative team</h2>
                         <p style={{ color: "#7D8188" }} className="my-3">At the start of every project, we cusomizea detailed project plan together with you set exectationns ensure alighment and define key milestones and deiverbles.</p>
