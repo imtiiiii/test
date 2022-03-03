08bde47575720a53f1cd61df9f373ce2b6d14ea8
Imtiaz Ahmed <imtiazahmed026@gmail.com>
2022-03-03 14:33:38 +0600

    fixing responsiveness

--- src/Components/Brands/Brands.js
+++ src/Components/Brands/Brands.js
@@ -12,21 +12,21 @@ const Brands = () => {
 
             <Container className=' py-5 my-5 rounded-pill mx-auto ' style={{ backgroundColor: "#E2EDDF" }}>
 
-                <Row className='  d-flex justify-content-center py-2 align-items-center  '>
-                    <Col lg={2} sm={12} className="py-2 ">
-                        <img alt="brands" src={google} className="mx-3 border border-2" style={{ width: "140px", height: "40px" }} />
+                <Row className=' py-3 d-flex justify-content-center align-item-center'>
+                    <Col lg={2} sm={12} className="py-3 d-flex justify-content-center align-item-center">
+                        <img alt="brands" src={google} className="mx-3  " style={{ width: "140px", height: "40px" }} />
                     </Col>
-                    <Col lg={2} sm={12} className="py-2">
-                        <img alt="brands" src={slack} className="mx-3 border border-2 " style={{ width: "140px", height: "40px" }} />
+                    <Col lg={2} sm={12} className="py-3 d-flex justify-content-center align-item-center">
+                        <img alt="brands" src={slack} className="mx-3   " style={{ width: "150px", height: "40px" }} />
                     </Col>
-                    <Col lg={2} sm={12} className="py-2">
-                        <img alt="brands" src={agworld} className="mx-3 border border-2" style={{ width: "140px", height: "40px" }} />
+                    <Col lg={2} sm={12} className="py-3 d-flex justify-content-center align-item-center">
+                        <img alt="brands" src={agworld} className="mx-3  " style={{ width: "140px", height: "40px" }} />
                     </Col>
-                    <Col lg={3} sm={12} className="py-2">
-                        <img alt="brands" src={riotinto} className="mx-3 border border-2" style={{ width: "140px", height: "40px" }} />
+                    <Col lg={2} sm={12} className="py-3 d-flex justify-content-center align-item-center">
+                        <img alt="brands" src={riotinto} className="mx-3 " style={{ width: "140px", height: "40px" }} />
                     </Col>
-                    <Col lg={2} sm={12} className="py-2" >
-                        <img alt="brands" src={heroku} className="mx-3 border border-2" style={{ width: "140px", height: "40px" }} />
+                    <Col lg={2} sm={12} className="py-3 d-flex justify-content-center align-item-center" >
+                        <img alt="brands" src={heroku} className="mx-3 " style={{ width: "140px", height: "40px" }} />
                     </Col>
 
 
